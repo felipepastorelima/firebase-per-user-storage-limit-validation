0fc2122c3eca5fac67aabd0ff7fa69583b33a57c
felipepastorelima <felipepastoree@gmail.com>
2017-08-15 21:03:09 -0300

    Indentation

--- functions/index.js
+++ functions/index.js
@@ -25,84 +25,60 @@ const storageBucket = `${process.env.GCLOUD_PROJECT}.appspot.com`;
 * Generates a upload token with the storage left information
 * that will be used to evaluate if the file sent doesn't exceed the quota
 */
-exports.uploadToken = functions
-  .https
-  .onRequest((request, response) => {
+exports.uploadToken = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
-      Authentication
-        .extractUidFromToken(request.query.token)
-        .then((uid) => {
-          return Storage
-            .storageLeftInBytes(uid)
-            .then(storageLeftInBytes => {
-              const metadata = {
-                storageLeftInBytes: Number(storageLeftInBytes)
-              };
+      Authentication.extractUidFromToken(request.query.token).then((uid) => {
+        return Storage.storageLeftInBytes(uid).then(storageLeftInBytes => {
+          const metadata = {
+            storageLeftInBytes: Number(storageLeftInBytes)
+          };
 
-              return admin
-                .auth()
-                .createCustomToken(uid, metadata)
-                .then((token) => {
-                  response.send({ token });
-                });
-            });
-        }).catch(function (error) {
-          console.error(error);
-          response.send(403);
+          return admin.auth().createCustomToken(uid, metadata).then((token) => {
+            response.send({ token });
+          });
         });
+      }).catch(function (error) {
+        console.error(error);
+        response.send(403);
+      });
     });
 });
 
 /*
 * Return the user's storage left in bytes
 */
-exports.storageLeftInBytes = functions
-  .https
-  .onRequest((request, response) => {
-    cors(request, response, () => {
-      Authentication
-        .extractUidFromToken(request.query.token)
-        .then((uid) => {
-          return Storage
-            .storageLeftInBytes(uid)
-            .then(storageLeftInBytes => {
-              response.send({ storageLeftInBytes });
-            });
-        }).catch(function (error) {
-          console.error(error);
-          response.send(403);
-        });
+exports.storageLeftInBytes = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    Authentication.extractUidFromToken(request.query.token).then((uid) => {
+      return Storage.storageLeftInBytes(uid).then(storageLeftInBytes => {
+        response.send({ storageLeftInBytes });
+      });
+    }).catch(function (error) {
+      console.error(error);
+      response.send(403);
     });
+  });
 });
 
 /*
 * Delete all user's files
 */
-exports.deleteAllFiles = functions
-  .https
-  .onRequest((request, response) => {
-    cors(request, response, () => {
-      Authentication
-        .extractUidFromToken(request.query.token)
-        .then((uid) => {
-          return Storage
-            .deleteAllFiles(uid)
-            .then(() => response.send(200));
-        }).catch(function (error) {
-          console.error(error);
-          response.send(403);
-        });
+exports.deleteAllFiles = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    Authentication.extractUidFromToken(request.query.token).then((uid) => {
+      return Storage.deleteAllFiles(uid).then(() => response.send(200));
+    }).catch(function (error) {
+      console.error(error);
+      response.send(403);
     });
+  });
 });
 
 class Authentication {
   static extractUidFromToken(token) {
-     return admin
-        .auth()
-        .verifyIdToken(token)
-        .then((decodedToken) => {
-          return decodedToken.uid;
-        });
+    return admin.auth().verifyIdToken(token).then((decodedToken) => {
+      return decodedToken.uid;
+    });
   }
 }
 
@@ -120,53 +96,44 @@ class Profile {
   }
 
   static of (uid) {
-    return admin
-      .database()
-      .ref(`users-profiles/${uid}`)
-      .once('value')
-      .then(snapshot => {
-        return snapshot.val() || this.none;
-      })
+    return admin.database().ref(`users-profiles/${uid}`).once('value').then(snapshot => {
+      return snapshot.val() || this.none;
+    })
   }
 }
 
 class Storage {
   static storageLeftInBytes(uid) {
-    return Profile.of(uid)
-      .then(profile => {
-        return Storage
-          .storageInBytes(uid)
-          .then(storageInBytes => {
-            const storageLimitInBytes = Storage.storageLimitInBytes(profile);
-            return storageLimitInBytes - storageInBytes;
-        })
+    return Profile.of(uid).then(profile => {
+      return Storage.storageInBytes(uid).then(storageInBytes => {
+        const storageLimitInBytes = Storage.storageLimitInBytes(profile);
+        return storageLimitInBytes - storageInBytes;
+      })
     });
   }
 
   static storageInBytes(uid) {
     return new Promise((resolve, reject) => {
-      storage
-        .bucket(storageBucket)
-        .getFiles(
-          { query: { prefix: uid } },
-          (err, files) => {
-            if (err) {
-              reject(err);
-              return;
-            }
-
-            if (!files || !files.length) {
-              resolve(0);
-              return;
-            }
-
-            resolve(
-              files
-                .map(file => Number(file.metadata.size))
-                .reduce((a, b) => a + b, 0)
-            );
+      storage.bucket(storageBucket).getFiles(
+        { query: { prefix: uid } },
+        (err, files) => {
+          if (err) {
+            reject(err);
+            return;
           }
-        );
+
+          if (!files || !files.length) {
+            resolve(0);
+            return;
+          }
+
+          resolve(
+            files
+              .map(file => Number(file.metadata.size))
+              .reduce((a, b) => a + b, 0)
+          );
+        }
+      );
     });
   }
 
@@ -186,33 +153,31 @@ class Storage {
 
   static deleteAllFiles(uid) {
     return new Promise((resolve, reject) => {
-      storage
-        .bucket(storageBucket)
-        .getFiles(
-          { query: { prefix: uid } },
-          (err, files) => {
-            if (err) {
-              reject(err);
-              return;
-            }
-
-            if (!files || !files.length) {
-              resolve();
-              return;
-            }
-
-            Promise.all(
-              files.map(file => file.delete())
-            )
-              .then(() => {
-                resolve();
-              })
-              .catch((error) => {
-                console.error(error);
-                reject(error);
-              });
+      storage.bucket(storageBucket).getFiles(
+        { query: { prefix: uid } },
+        (err, files) => {
+          if (err) {
+            reject(err);
+            return;
           }
-        );
+
+          if (!files || !files.length) {
+            resolve();
+            return;
+          }
+
+          Promise.all(
+            files.map(file => file.delete())
+          )
+            .then(() => {
+              resolve();
+            })
+            .catch((error) => {
+              console.error(error);
+              reject(error);
+            });
+        }
+      );
     });
   }
 }
